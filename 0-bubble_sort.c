92360e2d331b26442ede09ece38c14e6de0d78d2
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-09-05 22:22:44 -0500

    correction betty errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,7 +2,7 @@
 
 /**
  * bubble_sort - sorts an array of integers in ascending order using
- * the Bubble sort algorithm 
+ * the Bubble sort algorithm
  * @array: array
  * @size: size of the array
  * Return: nothing
