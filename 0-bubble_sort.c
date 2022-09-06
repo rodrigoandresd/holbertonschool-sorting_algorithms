c5204122a064daf4598be1d660b94287932ada6d
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-09-05 22:19:35 -0500

    correction betty error and NULL array

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,24 +10,26 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j, flag;
-    int temp;
+	size_t i, j, flag;
+	int temp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        flag = 0;
-        for (j = 0; j < size - 1 - i; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-                flag = 1;
-            }
-        }
-        if (flag == 0)
-            break;
-    }
+	if (array == NULL)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		flag = 0;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+			break;
+	}
 }
