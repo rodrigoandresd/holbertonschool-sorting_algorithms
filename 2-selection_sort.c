b954b52d2b0a8533f4f30cf93bd197f6aa50e0ab
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-09-06 23:00:18 -0500

    add task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array of integers in ascending order using 
+ * selection_sort - sorts an array of integers in ascending order using
  * the Selection sort algorithm
  * @array: array
  * @size: size of array
